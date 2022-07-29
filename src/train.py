bd295f7232350c80ed6e0d4365c9112bbf1ed399
Phil Michaels <93264586+PJMichaels@users.noreply.github.com>
2022-07-29 08:25:06 -0400

    Finished pipeline through train stage

--- src/train.py
+++ src/train.py
@@ -1,16 +1,37 @@
-from sklearn.model_selection import train_test_split
+import yaml
+from pathlib import Path
+import pandas as pd
+import numpy as np
 from sklearn.linear_model import LogisticRegression
+import pickle
 
+
+try:
+    params = yaml.safe_load(open("params.yaml"))["train"]
+    gen_params = yaml.safe_load(open("params.yaml"))["general"]
+    max_iterations = params['max_iter']
+    y_id = gen_params['y']
+    random_seed = gen_params['seed']
+    # mol_rep = params['mol_representation']
+except:
+    print('There is a problem during import of params.yaml for train.py')
+    exit()
 
 ### from YAML import split params or maybe eventually cross val info as well as random state
 ### random state could also be hard coded maybe
 ### YAML PARAMS will also either need to note X and Y column names to know what labels are
 
-# split the data:
-X=[generate_fingerprint(mol,2,1024) for mol in df['smiles']]
-y=df['HIV_active'].to_list()
+train_path = Path.cwd() / "data" / "featurized" / "train.csv"
+artifacts_dir = Path.cwd() / "pipeline_artifacts"
+if not artifacts_dir.exists():
+    artifacts_dir.mkdir()
+model_path = artifacts_dir / "model.pkl"
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+train_df = pd.read_csv(train_path)
+
+X_train = train_df.iloc[:,:-1]
+y_train = list(train_df[y_id])
+
 
 
 ### will need to find a way to make model import into a variable as well and ensure
@@ -22,9 +43,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 ### params may be quite different for each model
 
 ### take a time stamp here so we can also note model train time if not captured elsewhere
+print("\nBeggining to train model")
+clf = LogisticRegression(random_state= random_seed ,solver='lbfgs',max_iter= max_iterations, verbose= True)
+clf.fit(X_train, y_train)
 
-clf = LogisticRegression(random_state=0,solver='lbfgs',max_iter=1000)
-clf.fit(X_train,y_train)
+print(f"\nWriting model to filepath:\n{model_path}")
+pickle.dump(clf, open(model_path, 'wb'))
 
 ### take another time stamp and calculate/record delta for metrics
 ### pickle model here
