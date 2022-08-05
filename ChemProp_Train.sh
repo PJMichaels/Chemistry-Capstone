c9d18c82ae0ad2b26909d1650675ef435b1cd4b1
David <dunstandavid0@gmail.com>
2022-08-05 18:00:47 -0400

    added chem prop training file

--- ChemProp_Train.sh
+++ ChemProp_Train.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
 echo training_chemprop
+chemprop_train \
+    --data_path ~/Chemistry-Capstone/data_split_cleaned/bace_train.csv \
+    --dataset_type classification \
+    --save_dir ~/Chemistry-Capstone/Complex_Models/bace \
+    --epochs 50 \
+    --extra_metrics accuracy f1 binary_cross_entropy \
+    --metric auc \
+    --target_columns active\
+    --smiles_columns mol \
+    --num_folds 5 \
+    --split_sizes [0.8,0.2,0] \
+    --separate_val_path ~/Chemistry-Capstone/data_split_cleaned/bace_validate.csv\
+    --features_generator morgan
 
-chemprop_train --data_path ~/Chemistry-Capstone/HIV-2.csv --dataset_type classification --save_dir Chemistry-~/Capstone/test_chemprop --epochs 50 --extra_metrics accuracy f1 binary_cross_entropy --metric auc --target_columns HIV_active --smiles_columns smiles --num_folds 5
+echo predicting_bace_training
+chemprop_predict \
+    --test_path ~/Chemistry-Capstone/data_split_cleaned/bace_train.csv \
+    --preds_path ~/Chemistry-Capstone/Complex_Models/Predictions/bace_train_pred.csv \
+    --checkpoint_dir ~/Chemistry-Capstone/Complex_Models/bace \
+    --smiles_columns mol \
+    --features_generator morgan
+
+echo predicting_bace_validation
+chemprop_predict \
+    --test_path ~/Chemistry-Capstone/data_split_cleaned/bace_validate.csv \
+    --preds_path ~/Chemistry-Capstone/Complex_Models/Predictions/bace_validate_pred.csv \
+    --checkpoint_dir ~/Chemistry-Capstone/Complex_Models/bace \
+    --smiles_columns mol \
+    --features_generator morgan
