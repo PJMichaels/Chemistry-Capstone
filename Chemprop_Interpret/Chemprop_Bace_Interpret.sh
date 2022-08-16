621ace96c9f65bfc0169468105880c6a882235cc
David <dunstandavid0@gmail.com>
2022-08-16 10:44:50 -0400

    added script to try chemprop interpretation

---
+++ Chemprop_Interpret/Chemprop_Bace_Interpret.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo bace
+
+
+echo interpreting_chemprop_Bace
+
+chemprop_interpret \
+    --data_path ~/Chemistry-Capstone/Chemprop_Interpret/cluster_4.csv\
+    --checkpoint_dir ~/Chemistry-Capstone/Complex_Models/bace-cluster-hyperopt \
+    --property_id 1 \
+    --num_workers 1 \
+    --smiles_columns mol\
+    --features_generator morgan\
+
+
